192a1f2f4dd6d6a5363a198badb5bbe05220d2ac
Dominic Pham <dmncpham@Dominics-MacBook-Air.local>
2018-09-06 17:05:01 -0700

    Bulk commit

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Dominic Pham on 9/5/18.
+//  Copyright © 2018 Dominic Pham. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    
+
+}
